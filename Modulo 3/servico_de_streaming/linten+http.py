cd8d547bf16d1081e9bfee23bd416185fcb01e32
bms_007 <168493571+Bernardinomarques007@users.noreply.github.com>
2025-12-10 14:41:53 -0300

    ....

--- Modulo 3/servico_de_streaming/linten+http.py
+++ Modulo 3/servico_de_streaming/linten+http.py
@@ -3,14 +3,13 @@ import psycopg2
 import psycopg2.extensions
 import select
 import os
-from flask import render_template
+from flask import render_template, request, jsonify
 
 app = flask.Flask(__name__)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 def stream_messages(channel):
-    conn = psycopg2.connect(DATABASE_URL)
+    conn = psycopg2.connect(database='postgres', user='postgres',
+                            password='1234', host='localhost')
     conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 
     curs = conn.cursor()
@@ -27,11 +26,29 @@ def stream_messages(channel):
 def get_messages(channel):
     return flask.Response(stream_messages(channel), mimetype='text/event-stream')
 
+@app.route("/message/new", methods=['POST'])
+def new_message():
+    canal = request.args['id']
+    fonte = request.args['source']
+    msg = request.args['message']
 
+    try:
+        conn = psycopg2.connect(database='postgres', user='postgres',
+                                password='1234', host='localhost' )
+        
+        curs = conn.cursor()
+        curs.execute("INSERT INTO message(channel, source, content) VALUES(%s, %s, %s)", (canal, fonte, msg))
+        conn.commit()
+        conn.close()
+        return jsonify({'message': 'Conteúdo enviado com sucesso'}, 200)
+
+    except ConnectionRefusedError as e:
+        print('ERRO DE CONEXÃO', e)
+        return jsonify({'error': f'erro de servidor {e}'}, 500)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
